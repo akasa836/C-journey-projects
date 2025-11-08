438d6e48f4962c18ca45b4b3e16bbeb352148b4e
Proshanto Das <proshantodasvd@gmail.com>
2025-11-08 09:21:40 +0530

    Add files via upload

---
+++ temperature programing.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+	
+	char choice ='\0';
+	float Fahrenheit =0.0f;
+	float Celsius =0.0f;
+	
+	printf("temperature convergen program ");
+	printf("c.  Celsius to Fahrenheit");
+	printf("f .  ferenhiet to Celsius\n");
+	printf ("is the temp is Celsius (C) or Fahrenheit (F)?: ");
+	scanf("%c" , &choice);
+	
+	
+	if(choice =='c'){
+		printf("enetr the temp in celsius");
+		scanf("%f", &Celsius);
+		Fahrenheit = (Celsius*9/5)+32;
+		printf("%f Celsius is equal to %f Fahrenheit\n" , Celsius,Fahrenheit);
+		
+	}
+	else if (choice =='f'){
+		printf("enter the temperature in Fahrenheit:  ");
+		scanf("%f", &Fahrenheit);
+		Celsius= (Fahrenheit -32)*5/9;
+		printf("%f Fahrenheit is equal to %f Celsius\n" , Fahrenheit , Celsius);
+	}
+	else{
+		printf("invalide choice please select c or F\n");
+	}
+	
+	
+	return 0;
+}
