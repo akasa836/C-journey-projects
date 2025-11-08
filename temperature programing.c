7c0c299264091d49eda8a024ec8de854623c7dca
Proshanto Das <proshantodasvd@gmail.com>
2025-11-08 09:25:20 +0530

    Refactor temperature conversion program in C

--- temperature programing.c
+++ temperature programing.c
@@ -1,4 +1,7 @@
+// temperature programming  using c 
+
 #include<stdio.h>
+
 int main(){
 	
 	char choice ='\0';
@@ -6,29 +9,30 @@ int main(){
 	float Celsius =0.0f;
 	
 	printf("temperature convergen program ");
-	printf("c.  Celsius to Fahrenheit");
-	printf("f .  ferenhiet to Celsius\n");
-	printf ("is the temp is Celsius (C) or Fahrenheit (F)?: ");
+	printf("c. Celsius to Fahrenheit");
+	printf("f .ferenhiet to Celsius\n");
+	printf ("is the temp is Celsius (C) or Fahrenheit (f)?: ");
 	scanf("%c" , &choice);
 	
 	
-	if(choice =='c'){
+	if(choice =='c'){								// Celsius to Fahrenheit
 		printf("enetr the temp in celsius");
 		scanf("%f", &Celsius);
 		Fahrenheit = (Celsius*9/5)+32;
 		printf("%f Celsius is equal to %f Fahrenheit\n" , Celsius,Fahrenheit);
 		
 	}
-	else if (choice =='f'){
+	else if (choice =='f'){							//	ferenhiet to Celsius
 		printf("enter the temperature in Fahrenheit:  ");
 		scanf("%f", &Fahrenheit);
 		Celsius= (Fahrenheit -32)*5/9;
 		printf("%f Fahrenheit is equal to %f Celsius\n" , Fahrenheit , Celsius);
 	}
 	else{
-		printf("invalide choice please select c or F\n");
+		printf("invalide choice please select c or f\n");
 	}
 	
 	
 	return 0;
 }
+
